7e8fa014aeec9abdc35853124ac2544d4a7894da
Medhi <medhi.foulgoc@gmail.com>
2020-11-01 20:31:27 +0100

    last update

--- Medhi-Abdallah TP Particle/Assets/Scripts/ParticleManipulator.cs
+++ Medhi-Abdallah TP Particle/Assets/Scripts/ParticleManipulator.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using JetBrains.Annotations;
 using UnityEditor;
@@ -7,13 +8,6 @@ using Matrix4x4 = System.Numerics.Matrix4x4;
 
 public struct Particle
 {
-    public Particle(Vector3 newPos, Vector3 newVelocity, float newLifeTime)
-    {
-        position = newPos;
-        velocity = newVelocity;
-        life = newLifeTime;
-    }
-    
     public Vector3 position;
     public Vector3 velocity;
     public float life;
@@ -23,74 +17,103 @@ public class ParticleManipulator : MonoBehaviour
 {
     
     [SerializeField] private Camera myCamera;
-    [SerializeField] private GameObject cube;
+    //[SerializeField] private GameObject cube;
     [SerializeField] private ComputeShader myComputeShader;
     [SerializeField] private Material myCubeMaterial;
 
 
-    private ComputeBuffer myComputeBuffer;
+    private ComputeBuffer _myComputeBuffer;
     private Vector3 _mousePosition;
     private Vector3 _newCubePosition;
     private Vector3 _cameraPosition;
+    private Particle[] _myParticles;
+    private int _nbThread;
+    
     private float[] output = new float[3];
 
-    int kernelHandle;
-    private RenderTexture tex;
+    private int _kernelHandle;
+    private RenderTexture _tex;
     
     // Start is called before the first frame update
-
-    void RunShader()
-    {
-        
-        kernelHandle = myComputeShader.FindKernel("CSMain");
-        tex = new RenderTexture(256, 256, 24);
-        tex.enableRandomWrite = true;
-        tex.Create();
-        
-    }
-
-    void UpdateShader()
-    {
-        
-        myComputeBuffer = new ComputeBuffer(output.Length, 3*4);
-        myComputeBuffer.SetData(output);
-        myComputeShader.SetFloat("time", Time.deltaTime);
-        myComputeShader.SetVector("newPos", _newCubePosition);
-        myComputeShader.SetTexture(kernelHandle, "Result", tex);
-        myComputeShader.SetBuffer(kernelHandle, "debugger", myComputeBuffer);
-        myComputeShader.Dispatch(kernelHandle, 256/8, 256/8, 1);
-        myCubeMaterial.SetTexture("_MainTex", tex);
-        myComputeBuffer.GetData(output);
-        
-        print("X : " + output[0] + " Y : " + output[1] + " Z : " + output[2]);
-        
-        
-
-    }
-    
     void Start()
     {
-        
         RunShader();
-        Particle tst = new Particle(new Vector3(0, 0, 0), new Vector3(0, 0, 0), 15.0f);
+        //Particle tst = new Particle{ position = new Vector3(0, 0, 0), velocity = new Vector3(0, 0, 0), life = 15.0f};
     }
 
     // Update is called once per frame
     void Update()
     {
-        
         _cameraPosition = myCamera.transform.position;
         _mousePosition = Input.mousePosition;
         _newCubePosition = myCamera.ScreenToWorldPoint(new Vector3(_mousePosition.x, _mousePosition.y, -_cameraPosition.z));
        
-        Graphics.DrawProcedural(MeshTopology.Points, 1, 50);
+        //Graphics.DrawProceduralNow(MeshTopology.Points, 1, 1);
         
-        ParticleSystem tst = new ParticleSystem();
-        tst.is;
+        //ParticleSystem tst = new ParticleSystem();
+        //tst.is;
         
-        cube.transform.position = _newCubePosition;
-        
-        UpdateShader();
+        //cube.transform.position = _newCubePosition;
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            UpdateShader();
+        }
 
+        if (_myComputeBuffer == null)
+            return;
+        myComputeShader.SetFloat("deltaTime", Time.deltaTime);
+        myComputeShader.SetFloat("particleSize", 0.5f);
+        myComputeShader.SetVector("mousePosition", _newCubePosition);
+        myComputeShader.SetBuffer(_kernelHandle, "buffer", _myComputeBuffer);
+        myComputeShader.Dispatch(_kernelHandle, _nbThread, 1, 1);
     }
+
+    private void OnRenderObject()
+    {
+        if (_myComputeBuffer != null)
+        {
+            _myComputeBuffer.GetData(_myParticles);
+            myCubeMaterial.SetBuffer("_Buffer", _myComputeBuffer);
+            myCubeMaterial.SetPass(0);
+            Graphics.DrawProceduralNow(MeshTopology.Points, 1, _myParticles.Length);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        if (_myComputeBuffer != null)
+        {
+            _myComputeBuffer.Release();
+        }
+    }
+
+    void RunShader()
+    {
+        _kernelHandle = myComputeShader.FindKernel("CSMain");
+        _myParticles = new Particle[0];
+    }
+
+    void UpdateShader()
+    {
+        print("Bonjour");
+        var newParticles = new Particle[10000];
+        for (int i = 0; i < newParticles.Length; i++)
+        {
+            newParticles[i] = new Particle{position = new Vector3(_mousePosition.x, _mousePosition.y, -_cameraPosition.z), velocity = new Vector3(0, 0, 0), life = 5f};
+        }
+        List<Particle> tempParticle = new List<Particle>(_myParticles.Length + newParticles.Length);
+        tempParticle.AddRange(_myParticles);
+        tempParticle.AddRange(newParticles);
+        _myParticles = tempParticle.ToArray();
+        _nbThread = Mathf.CeilToInt((float)_myParticles.Length / 256);
+        if(_myComputeBuffer != null)
+            _myComputeBuffer.Release();
+        
+        print(_myParticles.Length);
+        
+        _myComputeBuffer = new ComputeBuffer(_myParticles.Length, 7 * sizeof(float));
+        _myComputeBuffer.SetData(_myParticles);
+    }
+    
+    
 }
